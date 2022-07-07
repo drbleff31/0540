3455a4d59617490c77845b11652ecf48797ac810
drbleff31 <107926351+drbleff31@users.noreply.github.com>
2022-07-07 10:24:51 +0300

    update bot_ya.js
    добавлено чуток функциональности той другой пятой десятой

--- Bot_ya.js
+++ Bot_ya.js
@@ -8,26 +8,51 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
-let keywords = ["как работает скрипт на javascript", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в Wordpress"];
+let keywords = ["купля продажа автомобиля", "автомобили купить", "подержанные авто"];
 let keyword = keywords[getRandom(0, keywords.length)];
 let links = document.links;
 let googleInput = document.getElementsByName("q")[0];
 let btnK = document.getElementsByName("btnK")[0];
 
+
+
+
 if (btnK !== undefined) {
-	googleInput.value = keyword;
-	btnK.click();
-} else {
-	for (let i = 0; i < links.length; i++) {
-		if (links[i].href.includes("mozilla.org")) {
-			let link = links[i];
-			console.log("Нашел строку " + links[i]);
-			link.click();
-			break;
+	let i = 0;
+	let timerId = setInterval(()=> {
+		googleInput.value += keyword[i];
+		i++;
+		if(i == keyword.length) {
+			clearInterval(timerId);
+			btnK.click();
+
+		}
+	},500)
+
+	} else {
+		let nextGooglePage = true;
+		for (let i = 0; i < links.length; i++) {
+			if (links[i].href.includes("auto.ru")) {
+				let link = links[i];
+				nextGooglePage = false;
+				console.log("Нашел строку " + links[i]);
+				setTimeout(()=>{
+					link.click();
+				},getRandom(2000, 5000))
+
+				break;
+			}
+		} 
+		if(document.querySelector(".YyVfkd").innerText == "5") {
+			 nextGooglePage = false;
+			 location.href = "https://www.google.com/";
+		}
+		if (nextGooglePage) {
+			setTimeout(()=>{
+			pnnext.click();
+			},getRandom(3000, 7000));
 		}
 	}
-}
-
 function getRandom (min, max) {
 	return Math.floor(Math.random()*(max - min) + min);
 }
